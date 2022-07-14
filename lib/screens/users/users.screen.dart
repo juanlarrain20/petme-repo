c7cd5953a5ac5135a5642b7262da6923e874d321
juanlarrain20 <juanlarra2000@gmail.com>
2022-07-14 18:27:22 -0400

    main

--- lib/screens/users/users.screen.dart
+++ lib/screens/users/users.screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:petme_v_0_0_1/models/models.dart';
+import 'package:petme_v_0_0_1/widgets/widgets.dart';
 
 class UsersScreen extends StatelessWidget {
   const UsersScreen({Key? key}) : super(key: key);
@@ -21,23 +22,51 @@ class UsersScreen extends StatelessWidget {
           backgroundColor: Colors.transparent,
         ),
         extendBodyBehindAppBar: true,
-        body: Stack(
-          children: [
-           SizedBox(
-            height: MediaQuery.of(context).size.height / 2,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                image: DecorationImage(
-                  image: NetworkImage(User.users[0].imageUrls[0]),
-                  fit: BoxFit.cover,
-
-                  
+        body: SizedBox(
+          height: MediaQuery.of(context).size.height / 2,
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 45),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    image: DecorationImage(
+                      image: NetworkImage(User.users[0].imageUrls[0]),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
               ),
+              Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 60.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const ChoiceButton(
+                  color: Colors.redAccent,
+                  icon: Icons.clear_rounded,
+                ),
+                const ChoiceButton(
+                  height: 80,
+                  width: 80,
+                  size: 30,
+                  color: Colors.green,
+                  icon: Icons.favorite,
+                  
+                ),
+                const ChoiceButton(
+                  color: Colors.black,
+                  icon: Icons.access_time_filled,
+                ),
+              ],
             ),
           ),
-          ],
+        )
+            ],
+          ),
         ),
       ),
     );
