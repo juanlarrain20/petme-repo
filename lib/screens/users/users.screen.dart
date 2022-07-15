44dc22dc3d4ebb98c873a6f4057d2a2f054e453c
juanlarrain20 <juanlarra2000@gmail.com>
2022-07-15 13:50:54 -0400

    users.page

--- lib/screens/users/users.screen.dart
+++ lib/screens/users/users.screen.dart
@@ -15,6 +15,7 @@ class UsersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final User user = User.users[0];
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -22,52 +23,88 @@ class UsersScreen extends StatelessWidget {
           backgroundColor: Colors.transparent,
         ),
         extendBodyBehindAppBar: true,
-        body: SizedBox(
-          height: MediaQuery.of(context).size.height / 1.7,
-          child: Stack(
-            children: [
-              //Imagen de usuario
-              Padding(
-                padding: const EdgeInsets.only(bottom: 45),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    image: DecorationImage(
-                      image: NetworkImage(User.users[0].imageUrls[0]),
-                      fit: BoxFit.cover,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 1.7,
+              child: Stack(
+                children: [
+                  //Imagen de usuario
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 45),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                          image: NetworkImage(User.users[0].imageUrls[0]),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              //Botones
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 60.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      const ChoiceButton(
-                        color: Colors.redAccent,
-                        icon: Icons.clear_rounded,
+                  //Botones
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 60.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          const ChoiceButton(
+                            color: Colors.redAccent,
+                            icon: Icons.clear_rounded,
+                          ),
+                          const ChoiceButton(
+                            height: 80,
+                            width: 80,
+                            size: 30,
+                            color: Colors.green,
+                            icon: Icons.favorite,
+                          ),
+                          const ChoiceButton(
+                            color: Colors.black,
+                            icon: Icons.access_time_filled,
+                          ),
+                        ],
                       ),
-                      const ChoiceButton(
-                        height: 80,
-                        width: 80,
-                        size: 30,
-                        color: Colors.green,
-                        icon: Icons.favorite,
-                      ),
-                      const ChoiceButton(
-                        color: Colors.black,
-                        icon: Icons.access_time_filled,
-                      ),
-                    ],
+                    ),
                   ),
-                ),
-              )
-            ],
-          ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${user.name}, ${user.age}',
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                  Text(
+                    '${user.bio}',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Text(
+                    'about',
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '${user.interest}',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
